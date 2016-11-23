51a1bfc3f79bda66263162b84c58406bf4411498
Mutefish0 <648262030@qq.com>
2016-11-23 11:54:14 +0800

    update

--- src/javascripts/components/App.js
+++ src/javascripts/components/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import Webhead from './common/Webhead'
 import Navi from './common/Navi'
+import Article from './views/Article'
+import Picture from './views/Picture'
+import Music from './views/Music'
+import Todo from './views/Todo'
 
 const Frame = ({children}) => (
   <div className='root'>
@@ -11,17 +15,14 @@ const Frame = ({children}) => (
   </div>
 )
 
-const Article = _ => (
-  <div>
-    文章
-  </div>
-)
-
 const App = () => (
   <Router history={browserHistory}>
     <Route path='/' component={Frame}>
       <IndexRoute component={Article} />
-      <Route path='article' component={Article} />
+      <Route path='/article' component={Article} />
+      <Route path='/picture' component={Picture} />
+      <Route path='/music' component={Music} />
+      <Route path='/todo' component={Todo} />
     </Route>
   </Router>
 )
--- src/javascripts/components/common/Navi.js
+++ src/javascripts/components/common/Navi.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navi = () => (
-  <div> 导航 </div>
+  <div className="common-navi">
+    <Link
+      to="/article"
+      className={`navi-item ${window.location.pathname=='/'? 'navi-item-active': ''}`}
+      activeClassName="navi-item-active"
+    >
+      文章
+    </Link>
+
+    <Link to="/picture" className="navi-item" activeClassName="navi-item-active">
+      图库
+    </Link>
+
+    <Link to="/music" className="navi-item" activeClassName="navi-item-active">
+      音乐
+    </Link>
+
+    <Link to="/todo" className="navi-item" activeClassName="navi-item-active">
+      备忘
+    </Link>
+  </div>
 )
 
 export default Navi
