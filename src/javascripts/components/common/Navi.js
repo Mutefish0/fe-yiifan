ac9d8c0b93bf61a4d85bed777e4ee24710f26d18
mutefish0 <648262030@qq.com>
2016-12-10 11:54:05 +0800

    navi bug fix

--- src/javascripts/components/common/Navi.js
+++ src/javascripts/components/common/Navi.js
@@ -3,29 +3,56 @@ import { Link } from 'react-router'
 import { connect } from 'react-redux'
 import { REQUEST_ARTICLES } from '../../actions/article'
 
-const Navi = ({ dispatch }) => (
-  <div className="common-navi">
-    <Link
-      to="/article"
-      className={`navi-item ${window.location.pathname=='/'? 'navi-item-active': ''}`}
-      activeClassName="navi-item-active"
-      onClick={_ => dispatch(REQUEST_ARTICLES)}
-    >
-      文章
-    </Link>
+class Navi extends React.Component {
+  static defaultProps = {
+    dispatch: _ => _
+  }
 
-    <Link to="/picture" className="navi-item" activeClassName="navi-item-active">
-      图库
-    </Link>
+  update() {
+    this.forceUpdate()
+  }
 
-    <Link to="/music" className="navi-item" activeClassName="navi-item-active">
-      音乐
-    </Link>
+  render() {
+    return (
+     <div className="common-navi">
+       <Link
+         to="/article"
+         className={`navi-item ${window.location.pathname=='/'? 'navi-item-active': ''}`}
+         activeClassName="navi-item-active"
+         onClick={_ => this.props.dispatch(REQUEST_ARTICLES)}
+       >
+         文章
+       </Link>
 
-    <Link to="/todo" className="navi-item" activeClassName="navi-item-active">
-      备忘
-    </Link>
-  </div>
-)
+       <Link
+        to="/picture"
+        className="navi-item"
+        activeClassName="navi-item-active"
+        onClick={this.update.bind(this)}
+      >
+         图库
+       </Link>
+
+       <Link
+        to="/music"
+        className="navi-item"
+        activeClassName="navi-item-active"
+        onClick={this.update.bind(this)}
+      >
+         音乐
+       </Link>
+
+       <Link
+        to="/todo"
+        className="navi-item"
+        activeClassName="navi-item-active"
+        onClick={this.update.bind(this)}
+       >
+         备忘
+       </Link>
+     </div>
+   )
+  }
+}
 
 export default connect()(Navi)
